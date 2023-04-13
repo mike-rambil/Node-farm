aa2eaa7983593be59527c6676a1844383d241a7e
Mike Rambil <mikerambil@gmail.com>
2023-04-13 02:21:30 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//Blocking, Sync
+
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we now about the avacado: ${textIn}.\nCreated on ${Date.now()}.`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+//Non Blocking, Async
+//FILES
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('File Written :)');
+//       });
+//     });
+//   });
+// });
+
+//SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from the SERVER...!!!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening on port 8000');
+});
